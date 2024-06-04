d6a2bd77196f26dd772b71b0b41e2d2c1858708d
ArmandoAB <a20110173@ceti.mx>
2024-06-03 20:36:19 -0600

    Funciones y lectura de reglas desde un archivo de texto

--- sistema.py
+++ sistema.py
@@ -1,18 +1,6 @@
-# %% [markdown]
-# ## Analizador Lexico
-# Un analizador lexico transforma una entrada en tokens
-
-# %%
-entrada = "si es de dia entonces mañana no es de noche"
-
-# %%
-tokens = entrada.lower().split()
-tokens
-
-# %%
 from enum import Enum
 
-class TipoToken(Enum):
+class TipoToken(Enum):  #sistema para clasificación de palabras y conectores
     O = 1
     Y = 2
     S = 3
@@ -20,29 +8,7 @@ class TipoToken(Enum):
     E = 5
     P = 6
 
-# %%
-lexemas = []
-for token in tokens:
-    if token == 'o':
-        lexemas.append({"valor" : token , "tipo": TipoToken.O })
-    elif token == 'y':
-        lexemas.append({"valor" : token , "tipo": TipoToken.Y})
-    elif token == 'si':
-        lexemas.append({"valor" : token , "tipo": TipoToken.S})
-    elif token == 'entonces':
-        lexemas.append({"valor" : token , "tipo": TipoToken.E})
-    elif token == 'no':
-        lexemas.append({"valor" : token , "tipo": TipoToken.N})
-    else :
-        lexemas.append({"valor" :token , "tipo": TipoToken.P })
-        
-lexemas
-
-# %% [markdown]
-# ## Analizador Sintactico
-
-# %%
-class EstadosAnalizador(Enum):
+class EstadosAnalizador(Enum):  #Sistema de guardado de Estados
     INICIO = 1
     PRECEDENTE = 2
     NEGACION = 3
@@ -56,108 +22,193 @@ class EstadosAnalizador(Enum):
     ERROR4 = 11 #se esperaba un atomo
     FIN = 12
 
-# %%
-EstadoActual = EstadosAnalizador.INICIO
+def Analizador_lexico(entrada : str) -> list:
+    lexemas.clear()
+    tokens = entrada.lower().split()
+    for token in tokens:
+        if token == 'o':
+            lexemas.append({"valor": token , "tipo": TipoToken.O })
+        elif token == 'y':
+            lexemas.append({"valor": token , "tipo": TipoToken.Y})
+        elif token == 'si':
+            lexemas.append({"valor": token , "tipo": TipoToken.S})
+        elif token == 'entonces':
+            lexemas.append({"valor": token , "tipo": TipoToken.E})
+        elif token == 'no':
+            lexemas.append({"valor": token , "tipo": TipoToken.N})
+        else :
+            lexemas.append({"valor":token , "tipo": TipoToken.P })
+            
+    return(lexemas)
 
-regla = ""
-pila = ""
-prec = 0
-pila = []
-prop = ""
+def Analizador_sintactico(lexemas : list):
+    regla = ""
+    pila = ""
+    prec = 0
+    pila = []
+    prop = ""
 
-for lexema in lexemas:
-    if EstadoActual == EstadosAnalizador.INICIO:
-        if lexema['valor'] == "si":
-            EstadoActual = EstadosAnalizador.PRECEDENTE
-            prec = 1
-        elif lexema['valor'] == "no":
-            EstadoActual = EstadosAnalizador.NEGACION
-        elif lexema['tipo'] == TipoToken.P:
-            EstadoActual == EstadosAnalizador.ATOMO
-            pila.append(lexema['valor'])
+    EstadoActual = EstadosAnalizador.INICIO
+
+    for lexema in lexemas:
+        if EstadoActual == EstadosAnalizador.INICIO:
+            if lexema['valor'] == "si":
+                EstadoActual = EstadosAnalizador.PRECEDENTE
+                prec = 1
+            elif lexema['valor'] == "no":
+                EstadoActual = EstadosAnalizador.NEGACION
+                if regla:
+                    regla += " "
+                regla += "~"
+            elif lexema['tipo'] == TipoToken.P:
+                EstadoActual = EstadosAnalizador.ATOMO
+                pila.append(lexema['valor'])
+            else:
+                EstadoActual = EstadosAnalizador.ERROR1
+
+        elif EstadoActual == EstadosAnalizador.PRECEDENTE:
+            if lexema['valor'] == "no":
+                EstadoActual = EstadosAnalizador.NEGACION
+                if regla:
+                    regla += " "
+                regla += "~"
+            elif lexema['tipo'] == TipoToken.P:
+                EstadoActual = EstadosAnalizador.ATOMO
+                pila.append(lexema['valor'])
+            else:
+                EstadoActual = EstadosAnalizador.ERROR2
+
+        elif EstadoActual == EstadosAnalizador.NEGACION:
+            if lexema['tipo'] == TipoToken.P:
+                EstadoActual = EstadosAnalizador.ATOMO
+                pila.append(lexema['valor'])
+            else:
+                EstadoActual = EstadosAnalizador.ERROR3
+
+        elif EstadoActual == EstadosAnalizador.CONSECUENTE:
+            if lexema['tipo'] == TipoToken.P:
+                EstadoActual = EstadosAnalizador.ATOMO
+                pila.append(lexema['valor'])
+            elif lexema['valor'] == "no":
+                EstadoActual = EstadosAnalizador.NEGACION
+                if regla:
+                    regla += " "
+                regla += "~"
+            else:
+                EstadoActual = EstadosAnalizador.ERROR2
+
+        elif EstadoActual == EstadosAnalizador.ATOMO:
+            if lexema['valor'] == "entonces":
+                EstadoActual = EstadosAnalizador.CONSECUENTE
+                prop = " ".join(pila)
+                if regla:
+                    regla += " "
+                regla += prop
+                if regla:
+                    regla += " "
+                regla += "->"
+                pila.clear()
+                prop = ""
+            elif lexema['valor'] == "y":
+                EstadoActual = EstadosAnalizador.CONJUNCION
+                prop = " ".join(pila)
+                if regla:
+                    regla += " "
+                regla += prop
+                if regla:
+                    regla += " "
+                regla += "&"
+                pila.clear()
+                prop = ""
+            elif lexema['valor'] == "o":
+                EstadoActual = EstadosAnalizador.DISYUNCION
+                prop = " ".join(pila)
+                if regla:
+                    regla += " "
+                regla += prop
+                if regla:
+                    regla += " "
+                regla += "V"
+                pila.clear()
+                prop = ""
+            elif lexema['tipo'] == TipoToken.P:
+                EstadoActual = EstadosAnalizador.ATOMO
+                pila.append(lexema['valor'])
+            else:
+                EstadoActual = EstadosAnalizador.ERROR3
+
+        elif EstadoActual == EstadosAnalizador.CONJUNCION:
+            if lexema['tipo'] == TipoToken.P:
+                EstadoActual = EstadosAnalizador.ATOMO
+                pila.append(lexema['valor'])
+            elif lexema['valor'] == "no":
+                EstadoActual = EstadosAnalizador.NEGACION
+                if regla:
+                    regla += " "
+                regla += "~"
+            else:
+                EstadoActual = EstadosAnalizador.ERROR2
+
+        elif EstadoActual == EstadosAnalizador.DISYUNCION:
+            if lexema['tipo'] == TipoToken.P:
+                EstadoActual = EstadosAnalizador.ATOMO
+                pila.append(lexema['valor'])
+            elif lexema['valor'] == "no":
+                EstadoActual == EstadosAnalizador.NEGACION
+                if regla:
+                    regla += " "
+                regla += "~"
+            else:
+                EstadoActual = EstadosAnalizador.ERROR2
+
+        elif EstadoActual == EstadosAnalizador.ERROR1:
+            print("se esperaba un 'si', 'no' o un atomo")
+            break
+        elif EstadoActual == EstadosAnalizador.ERROR2:
+            print("se esperaba un 'no' o un atomo")
+            break
+        elif EstadoActual == EstadosAnalizador.ERROR3:
+            print("se esperaba un 'y', 'o', 'entonces' o un atomo")
+            break
+        elif EstadoActual == EstadosAnalizador.ERROR4:
+            print("se esperaba un atomo")
+            break
+        elif EstadoActual == EstadosAnalizador.FIN:
+            break
         else:
-            EstadoActual = EstadosAnalizador.ERROR1
+            EstadoActual == EstadosAnalizador.FIN
 
-    elif EstadoActual == EstadosAnalizador.PRECEDENTE:
-        if lexema['valor'] == "no":
-            EstadoActual = EstadosAnalizador.NEGACION
-        elif lexema['tipo'] == TipoToken.P:
-            EstadoActual = EstadosAnalizador.ATOMO
-            pila.append(lexema['valor'])
+    if EstadoActual != (EstadosAnalizador.ERROR1 and EstadosAnalizador.ERROR2 and EstadosAnalizador.ERROR3 and EstadosAnalizador.ERROR4):
+        prop = " ".join(pila)
+        if regla and prop:
+            regla += " " + prop
         else:
-            EstadoActual = EstadosAnalizador.ERROR2
+            regla = prop
+        l_reglas.append(regla)
+        prop = ""
+        pila.clear()
+        regla = ""
+    
+    
+    return(l_reglas)
 
-    elif EstadoActual == EstadosAnalizador.NEGACION:
-        if lexema['tipo'] == TipoToken.P:
-            EstadoActual = EstadosAnalizador.ATOMO
-            pila.append(lexema['valor'])
-        else:
-            EstadoActual = EstadosAnalizador.ERROR3
+lexemas = []
+l_reglas = []
+Palabras = []
 
-    elif EstadoActual == EstadosAnalizador.CONSECUENTE:
-        if lexema['tipo'] == TipoToken.P:
-            EstadoActual = EstadosAnalizador.ATOMO
-            pila.append(lexema['valor'])
-        elif lexema['valor'] == "no":
-            EstadoActual = EstadosAnalizador.NEGACION
-        else:
-            EstadoActual = EstadosAnalizador.ERROR2
+with open("archivo.txt", "r") as archivo:
+    lineas = archivo.readlines()
 
-    elif EstadoActual == EstadosAnalizador.ATOMO:
-        if lexema['valor'] == "entonces":
-            EstadoActual = EstadosAnalizador.CONSECUENTE
-            prop = " ".join(pila)
-        elif lexema['valor'] == "y":
-            EstadoActual = EstadosAnalizador.CONJUNCION
-            prop = " ".join(pila)
-        elif lexema['valor'] == "o":
-            EstadoActual = EstadosAnalizador.DISYUNCION
-            prop = " ".join(pila)
-        elif lexema['tipo'] == TipoToken.P:
-            EstadoActual = EstadosAnalizador.ATOMO
-            pila.append(lexema['valor'])
-        else:
-            EstadoActual = EstadosAnalizador.ERROR3
+erease = "\n"
+enun = []
+for linea in lineas:
+    ent = linea.replace(erease, "")
+    enun.append(ent)
 
-    elif EstadoActual == EstadosAnalizador.CONJUNCION:
-        if lexema['tipo'] == TipoToken.P:
-            EstadoActual = EstadosAnalizador.ATOMO
-            pila.append(lexema['valor'])
-        elif lexema['valor'] == "no":
-            EstadoActual = EstadosAnalizador.NEGACION
-        else:
-            EstadoActual = EstadosAnalizador.ERROR2
+for frase in enun:
+    print(frase)
+    Palabras.clear()
+    Palabras = Analizador_lexico(frase)
+    Analizador_sintactico(Palabras)
 
-    elif EstadoActual == EstadosAnalizador.DISYUNCION:
-        if lexema['tipo'] == TipoToken.P:
-            EstadoActual = EstadosAnalizador.ATOMO
-            pila.append(lexema['valor'])
-        elif lexema['valor'] == "no":
-            EstadoActual == EstadosAnalizador.NEGACION
-        else:
-            EstadoActual = EstadosAnalizador.ERROR4
-
-    elif EstadoActual == EstadosAnalizador.ERROR1:
-        print("se esperaba un 'si', 'no' o un atomo")
-        break
-    elif EstadoActual == EstadosAnalizador.ERROR2:
-        print("se esperaba un 'no' o un atomo")
-        break
-    elif EstadoActual == EstadosAnalizador.ERROR3:
-        print("se esperaba un 'y', 'o', 'entonces' o un atomo")
-        break
-    elif EstadoActual == EstadosAnalizador.ERROR4:
-        print("se esperaba un atomo")
-        break
-    elif EstadoActual == EstadosAnalizador.FIN:
-        break
-    else:
-        EstadoActual == EstadosAnalizador.FIN
-    print(EstadoActual)
-    print(pila)
-    print(prop)
-
-# %%
-print(EstadoActual)
-
-
+l_reglas
